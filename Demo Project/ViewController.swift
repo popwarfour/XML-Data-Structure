ecc9b8f206682a3ed953a29caab3e84d8386b427
Anders <popwarfour2@gmail.com>
2017-08-17 15:21:40 -0400

    Initial Commit

---
+++ Demo Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo Project
+//
+//  Created by Jordan Rouille on 8/17/17.
+//  Copyright © 2017 Anders Melen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
